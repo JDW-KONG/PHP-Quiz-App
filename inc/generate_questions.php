31bbcfae01f2f25dae77dc05702c58140a59ab6c
jw <jw@localhost.localdomain>
2021-02-01 19:17:24 -0500

    updated question generator

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -10,13 +10,36 @@ if (!isset($_SESSION['questions'])) {
 
     // for each question
     for ($i=0;$i<$question_count;$i++) {
+        $used_numbers = [];
 
         // store question info in $_SESSION['questions']
         $_SESSION['questions'][$i]['leftAdder'] = rand(20, 100);
         $_SESSION['questions'][$i]['rightAdder'] = rand(20, 100);
         $_SESSION['questions'][$i]['correctAnswer'] = $_SESSION['questions'][$i]['leftAdder'] + $_SESSION['questions'][$i]['rightAdder'];
-        $_SESSION['questions'][$i]['firstIncorrectAnswer'] = $_SESSION['questions'][$i]['leftAdder'] + $_SESSION['questions'][$i]['rightAdder'] - rand(1, 10);
-        $_SESSION['questions'][$i]['secondIncorrectAnswer'] = $_SESSION['questions'][$i]['leftAdder'] + $_SESSION['questions'][$i]['rightAdder'] + rand(1, 10);
+
+
+        // set first incorrect answer to semi-random number
+        do {
+            $_SESSION['questions'][$i]['firstIncorrectAnswer'] = rand(-10, 10) + $_SESSION['questions'][$i]['correctAnswer'];
+        } while( 
+            // while first incorrect answer is in used numbers
+            in_array($_SESSION['questions'][$i]['firstIncorrectAnswer'], $used_numbers)
+        );
+
+        // add first incorrect answer to used numbers
+        array_push($used_numbers, $_SESSION['questions'][$i]['firstIncorrectAnswer']);
+
+
+        // set second incorrect answer to semi-random number
+        do {
+            $_SESSION['questions'][$i]['secondIncorrectAnswer'] = rand(-10, 10) + $_SESSION['questions'][$i]['correctAnswer'];
+        } while( 
+            // while second incorrect answer is in used numbers
+            in_array($_SESSION['questions'][$i]['secondIncorrectAnswer'], $used_numbers)
+        );
+
+        // add second incorrect answer to used numbers
+        array_push($used_numbers, $_SESSION['questions'][$i]['secondIncorrectAnswer']);
     }
 }
 
